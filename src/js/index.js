093536a1ee71a5e906e7bb3f60c4d19cb71e59b9
Todd Birchard <toddbirchard@gmail.com>
2022-11-01 03:58:23 -0400

    Misc nav & style fixes.

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,5 @@ window.addEventListener("load", function () {
     const x = e.pageX - slider.offsetLeft;
     const walk = (x - startX) * 3; //scroll-fast
     slider.scrollLeft = scrollLeft - walk;
-    console.log(walk);
   });
 });
