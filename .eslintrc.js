fe83ef1ef4511481a0e0a57544fa2e31b75adc23
Todd Birchard <toddbirchard@gmail.com>
2022-11-04 22:18:06 -0400

    Add linter.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-    parser: "@babel/eslint-parser",
-    parserOptions: {
-        ecmaVersion: 6,
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  plugins: ["import"],
+  extends: ["plugin:ghost/[config]"],
+  entry: {
+    global: path.resolve(__dirname, "./src/js/index.js"),
+  },
+  resolve: {
+    alias: {
+      Fonts: path.resolve(__dirname, "./assets/fonts/"),
+      Styles: path.resolve(__dirname, "./src/scss/"),
     },
-    plugins: [
-        `import`,
-      ],
-      entry: {
-        'global': path.resolve(__dirname, './src/js/index.js'),
-      },
-      resolve: {
-        alias: {
-          Fonts: path.resolve(__dirname, './assets/fonts/'),
-          Styles: path.resolve(__dirname, './src/scss/')
-        }
-    },
-  };
+  },
+};
