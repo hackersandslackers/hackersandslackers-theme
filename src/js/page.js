d09ad2452c87c6d6b1bde935f2a307d1594089a9
Todd Birchard <toddbirchard@gmail.com>
2022-11-19 22:45:32 -0500

    Add workaround for Facebook->Github links.

--- src/js/page.js
+++ src/js/page.js
@@ -1,3 +1,21 @@
 import "../scss/page.scss";
 import "../scss/series.scss";
 import "../scss/about.scss";
+
+window.addEventListener("load", function () {
+  fixGithubUrls();
+});
+
+// Workaround to repurpose Ghost Author `Facebook` links as `GitHub` links
+function fixGithubUrls() {
+  const githubLinks = document.querySelectorAll(
+    ".author-profile-social-link.github"
+  );
+
+  if (githubLinks != null) {
+    githubLinks.forEach((link) => {
+      const githubUrl = link.href.replace("https://www.facebook.com/", "");
+      link.href = githubUrl;
+    });
+  }
+}
