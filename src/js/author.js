34c20e7c36ada6f098db0aee18a30f57b1583720
Todd Birchard <toddbirchard@gmail.com>
2022-11-30 02:51:13 -0500

    Author github card attempt.

---
+++ src/js/author.js
@@ -0,0 +1,16 @@
+import GitHubCard from "github-profile-card";
+
+window.addEventListener("load", function () {
+  let githubUsername = process.env.GITHUB_USERNAME;
+
+  let widget = new GitHubCard({
+    username: githubUsername,
+    template: "#author-github-widget",
+    sortBy: "stars",
+    reposHeaderText: "Most starred",
+    maxRepos: 5,
+    hideTopLanguages: false,
+  });
+
+  widget.init();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,28 @@ const TerserPlugin = require(`terser-webpack-plugin`);
 
 module.exports = {
   entry: {
-    'main':  path.resolve(__dirname, `./src/js/index.js`),
-    'post': path.resolve(__dirname, `./src/js/post.js`),
-    'page': path.resolve(__dirname, `./src/js/page.js`),
+    main: path.resolve(__dirname, `./src/js/index.js`),
+    post: path.resolve(__dirname, `./src/js/post.js`),
+    page: path.resolve(__dirname, `./src/js/page.js`),
+    author: path.resolve(__dirname, `./src/js/author.js`),
   },
   plugins: [
-    new DotEnv({ path: './.env' }),
+    new DotEnv({ path: "./.env" }),
     new FontConfigWebpackPlugin(),
-    new MiniCssExtractPlugin({filename: "[name].css", chunkFilename: "[id].css"}),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
   ],
   mode: process.env.ENVIRONMENT,
   resolve: {
     alias: {
-      Styles: path.resolve(__dirname, './src/scss/')
-    }
+      Styles: path.resolve(__dirname, "./src/scss/"),
+    },
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './assets/built'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./assets/built"),
   },
   optimization: {
     minimize: true,
@@ -33,28 +37,29 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }, {
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
         test: /\.css$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: './assets/built'
-            }
+              publicPath: "./assets/built",
+            },
           },
-          "css-loader"
-        ]
+          "css-loader",
+        ],
       },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
